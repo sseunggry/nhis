147ce93203d480baef667af216a4bb1864064708
sychoi <sychoi@pulip.com>
2024-10-17 11:08:17 +0900

    페이지 수정

--- _res/nhis/nhis/js/main.js
+++ _res/nhis/nhis/js/main.js
@@ -37,19 +37,21 @@ $(document).stop().ready(function() {
     const $eleBanSwiperPlay = document.querySelector('.middle_group .swiper-button-play');
     const $eleBanSwiperStop = document.querySelector('.middle_group .swiper-button-stop');
 
-    $eleBanSwiperPlay.style.display = "none";
-
-    $eleBanSwiperPlay.addEventListener("click", () => {
-        eleBanSwiper.autoplay.start();
-        $eleBanSwiperStop.style.display = "";
-        $eleBanSwiperPlay.style.display = "none";
-    });
-
-    $eleBanSwiperStop.addEventListener("click", () => {
-        eleBanSwiper.autoplay.stop();
-        $eleBanSwiperStop.style.display = "none";
-        $eleBanSwiperPlay.style.display = "";
-    });
+	if($eleBanSwiperPlay){
+		$eleBanSwiperPlay.style.display = "none";
+	
+		$eleBanSwiperPlay.addEventListener("click", () => {
+			eleBanSwiper.autoplay.start();
+			$eleBanSwiperStop.style.display = "";
+			$eleBanSwiperPlay.style.display = "none";
+		});
+	
+		$eleBanSwiperStop.addEventListener("click", () => {
+			eleBanSwiper.autoplay.stop();
+			$eleBanSwiperStop.style.display = "none";
+			$eleBanSwiperPlay.style.display = "";
+		});
+	}
 
     //고객만족도 조사
     const eleBanSwiper2 = new Swiper('.news_group .swiper', {
@@ -74,19 +76,21 @@ $(document).stop().ready(function() {
     const $eleBanSwiperPlay2 = document.querySelector('.news_group .swiper-button-play');
     const $eleBanSwiperStop2 = document.querySelector('.news_group .swiper-button-stop');
 
-    $eleBanSwiperPlay2.style.display = "none";
-
-    $eleBanSwiperPlay2.addEventListener("click", () => {
-        eleBanSwiper2.autoplay.start();
-        $eleBanSwiperStop2.style.display = "";
-        $eleBanSwiperPlay2.style.display = "none";
-    });
-
-    $eleBanSwiperStop2.addEventListener("click", () => {
-        eleBanSwiper2.autoplay.stop();
-        $eleBanSwiperStop2.style.display = "none";
-        $eleBanSwiperPlay2.style.display = "";
-    });
+	if($eleBanSwiperPlay2){
+		$eleBanSwiperPlay2.style.display = "none";
+	
+		$eleBanSwiperPlay2.addEventListener("click", () => {
+			eleBanSwiper2.autoplay.start();
+			$eleBanSwiperStop2.style.display = "";
+			$eleBanSwiperPlay2.style.display = "none";
+		});
+	
+		$eleBanSwiperStop2.addEventListener("click", () => {
+			eleBanSwiper2.autoplay.stop();
+			$eleBanSwiperStop2.style.display = "none";
+			$eleBanSwiperPlay2.style.display = "";
+		});
+	}
 
     //SNS
     const eleBanSwiper3 = new Swiper('.sns .swiper', {
@@ -116,19 +120,21 @@ $(document).stop().ready(function() {
     const $eleBanSwiperPlay3 = document.querySelector('.sns .swiper-button-play');
     const $eleBanSwiperStop3 = document.querySelector('.sns .swiper-button-stop');
 
-    $eleBanSwiperPlay3.style.display = "none";
-
-    $eleBanSwiperPlay3.addEventListener("click", () => {
-        eleBanSwiper3.autoplay.start();
-        $eleBanSwiperStop3.style.display = "";
-        $eleBanSwiperPlay3.style.display = "none";
-    });
-
-    $eleBanSwiperStop3.addEventListener("click", () => {
-        eleBanSwiper3.autoplay.stop();
-        $eleBanSwiperStop3.style.display = "none";
-        $eleBanSwiperPlay3.style.display = "";
-    });
+	if($eleBanSwiperPlay3){
+		$eleBanSwiperPlay3.style.display = "none";
+	
+		$eleBanSwiperPlay3.addEventListener("click", () => {
+			eleBanSwiper3.autoplay.start();
+			$eleBanSwiperStop3.style.display = "";
+			$eleBanSwiperPlay3.style.display = "none";
+		});
+	
+		$eleBanSwiperStop3.addEventListener("click", () => {
+			eleBanSwiper3.autoplay.stop();
+			$eleBanSwiperStop3.style.display = "none";
+			$eleBanSwiperPlay3.style.display = "";
+		});
+	}
 
     //홍보존
     const eleBanSwiper4 = new Swiper('.marketing .swiper', {
@@ -153,19 +159,21 @@ $(document).stop().ready(function() {
     const $eleBanSwiperPlay4 = document.querySelector('.marketing .swiper-button-play');
     const $eleBanSwiperStop4 = document.querySelector('.marketing .swiper-button-stop');
 
-    $eleBanSwiperPlay4.style.display = "none";
-
-    $eleBanSwiperPlay4.addEventListener("click", () => {
-        eleBanSwiper4.autoplay.start();
-        $eleBanSwiperStop4.style.display = "";
-        $eleBanSwiperPlay4.style.display = "none";
-    });
-
-    $eleBanSwiperStop4.addEventListener("click", () => {
-        eleBanSwiper4.autoplay.stop();
-        $eleBanSwiperStop4.style.display = "none";
-        $eleBanSwiperPlay4.style.display = "";
-    });
+	if($eleBanSwiperPlay4){
+		$eleBanSwiperPlay4.style.display = "none";
+	
+		$eleBanSwiperPlay4.addEventListener("click", () => {
+			eleBanSwiper4.autoplay.start();
+			$eleBanSwiperStop4.style.display = "";
+			$eleBanSwiperPlay4.style.display = "none";
+		});
+	
+		$eleBanSwiperStop4.addEventListener("click", () => {
+			eleBanSwiper4.autoplay.stop();
+			$eleBanSwiperStop4.style.display = "none";
+			$eleBanSwiperPlay4.style.display = "";
+		});
+	}
 
     // swiper 배열처리
     $serviceSwiper_arr = [];
@@ -234,260 +242,6 @@ $(document).stop().ready(function() {
     $(document).on('click','.middle_group .title-justify button,.middle_group .check-layer > .btn-group button',function() {
         $(".middle_group .check-layer").toggleClass("on");
     });
-
-    //임시
-    function getAllStyle2() {
-        $('#wrap *').each((idx, el) => {
-            let htmlEl = '';
-            const $el = $(el);
-            const elTag = $el.prop('tagName');
-            // const elHeight = $el.outerHeight();
-            // const color = $el.css('color') == $('body').css('color') ? '' : rgbToHex($el.css('color'));
-            // const fontSize = $el.css('font-size') == $('body').css('font-size') ? '' : $el.css('font-size');
-            // const backgroundColor = $el.css('background-color') == $('body').css('background-color') ? '' : rgbToHex($el.css('background-color'));
-
-            // const styles = {
-            //     color: $el.css('color') == $('body').css('color') ? '' : rgbToHex($el.css('color')),
-            //     fontSize: $el.css('font-size') == $('body').css('font-size') ? '' : $el.css('font-size'),
-            //     backgroundColor: $el.css('background-color') == 'rgba(0, 0, 0, 0)' ? '' : rgbToHex($el.css('background-color'))
-            // };
-
-            // let color = styles.color ? `c: ${styles.color},` : '';
-            // let fontSize = styles.fontSize ? `${styles.fontSize},` : '';
-            // let backgroundColor = styles.backgroundColor ? `bg: ${styles.backgroundColor}` : '';
-            
-            // $el.css('position', 'relative');
-            if (elTag !== 'BR' && elTag !== 'I' && elTag !== 'IMG'){
-                // htmlEl += `
-                //     <p class="css-caption" style="position: absolute; font-size: 12px; color: #000; background-color: rgba(0, 0, 0, 0.5);">
-                //         ${color}${fontSize}${backgroundColor}
-                //     </p>
-                // `;
-                // if(color.length || fontSize.length || backgroundColor.length){
-                //     $el.append(htmlEl);
-                // }
-            }
-
-            // if (color || fontSize || backgroundColor) {
-            //     console.log(styles, $(el).css('color'), $(el).parent().css('color'));
-            // }
-            
-
-            const cssProperties = ['color', 'fontSize', 'backgroundColor'];
-            let cssValue = {};
-            $.each(cssProperties, (idx, property) => {
-                if (property == 'color' && $(el).css(property) !== $('body').css('color')){
-                    cssValue[property] = rgbToHex($(el).css(property));
-                }
-                if (property == 'fontSize' && $(el).css(property) !== $('body').css('font-size')){
-                    cssValue[property] = $(el).css(property);
-                }
-                if (property == 'backgroundColor' && $(el).css(property) !== 'rgba(0, 0, 0, 0)'){
-                    cssValue[property] = rgbToHex($(el).css(property));
-                }
-            });
-            if (cssValue.color || cssValue.fontSize || cssValue.backgroundColor){
-                if (elTag !== 'BR' && elTag !== 'I' && elTag !== 'IMG') {
-                    cssValue = JSON.stringify(cssValue).replace(/[{\"}]/g, '').replace(/,/g, ', ');
-                    htmlEl += `
-                        <p class="css-caption" style="position: absolute; font-size: 12px !important; color: #000 !important;; background-color: rgba(0, 0, 0, 0.5) !important; margin-top: 0 !important;">
-                            ${cssValue}
-                        </p>
-                    `;
-                    $el.append(htmlEl);
-                }
-            }
-        });
-
-        $('.css-caption').each((idx, el) => {
-            let elText = $(el).text().trim();
-            let elParentText = $(el).parent().text().trim();
-
-            while (elText == elParentText) {
-                el = $(el).parent();
-                // elParentCss = $(target).css(property);
-
-                if (el.length == 0) {
-                    return elText;
-                }
-            };
-            // if($(el).text().trim() == $(el).parent().text().trim()){
-            //     console.log(123);
-            // }
-            console.log(elText);
-        });
-
-        function removeDuplicateCss(target, property) {
-            let elCss = $(target).css(property);
-            let elParentCss = $(target).parent().css(property);
-            
-            while (elCss == elParentCss) {
-                target = $(target).parent();
-                elParentCss = $(target).css(property);
-
-                if(target.length == 0) {
-                    return elCss;
-                }
-            };
-            if( property.toLowerCase().includes('color') ){
-                elCss = rgbToHex(elCss);
-            }
-            return elCss;
-        }
-
-        $('.css-caption').each((idx, el) => {
-            // console.log($(this), $(el));
-            $(el).parent()
-            .on('mouseenter', () => {
-                $(el).css({ 'background-color':'#000', 'color': '#fff'});
-            })
-            .on('mouseleave', () => {
-                $(el).css({ 'font-size': '12px !important', 'color': '#000 !important', 'background-color':'rgba(0, 0, 0, 0.5) !important', 'margin-top': '0 !important'});
-            });
-        });
-    }
-    function getAllStyle1() {
-        const allElements = document.querySelectorAll('#wrap *');
-        const stylesArray = [];
-
-        allElements.forEach((el) => {
-            const elStyle = window.getComputedStyle(el);
-            const styles = {
-                tagName: el.tagName,
-                css: {}
-            };
-
-            for(let i = 0; i < elStyle.length; i++){
-                const property = elStyle[i];
-                if(property == 'fontSize' || property == 'backgroundColor'){
-                    styles.css[property] = elStyle.getPropertyValue(property);
-                } else if(property == 'color') {
-                    styles.css[property] = rgbToHex(elStyle.getPropertyValue(property));
-                }
-            }
-
-            stylesArray.push(styles);
-        });
-
-        return stylesArray;
-    }
-    function getAllStylePushHtml() {
-        const allElements = document.querySelectorAll('#wrap *');
-        const stylesArray = [];
-
-        allElements.forEach((el) => {
-            const elStyle = window.getComputedStyle(el);
-            const styles = {};
-
-            for(let i = 0; i < elStyle.length; i++){
-                const property = elStyle[i];
-                if(property == 'fontSize' || property == 'backgroundColor'){
-                    styles.css[property] = elStyle.getPropertyValue(property);
-                } else if(property == 'color') {
-                    styles.css[property] = rgbToHex(elStyle.getPropertyValue(property));
-                }
-            }
-
-            stylesArray.push(styles);
-            console.log(styles);
-        });
-
-        return stylesArray;
-    }
-    function getAllStyle() {
-        $('#wrap *').each((idx, el) => {
-            let htmlEl = '';
-            const $el = $(el);
-            const elTag = $el.prop('tagName');
-            const cssProperties = ['color', 'fontSize', 'backgroundColor'];
-            let cssValue = {};
-            const elHeight = $el.outerHeight();
-            
-            $.each(cssProperties, (idx, property) => {
-                const initialColor = $('body').css('color');
-                const initialFontSize = $('body').css('font-size');
-                const initialBackgroundColor = 'rgba(0, 0, 0, 0)';
-
-                if (property == 'color' && $(el).css(property) !== initialColor) {
-                    cssValue[property] = rgbToHex($(el).css(property));
-                }
-                if (property == 'fontSize' && $(el).css(property) !== initialFontSize) {
-                    cssValue[property] = $(el).css(property);
-                }
-                if (property == 'backgroundColor' && $(el).css(property) !== initialBackgroundColor) {
-                    cssValue[property] = rgbToHex($(el).css(property));
-                }
-                // cssValue[property] = rgbToHex($(el).css(property));
-            });
-            if (cssValue.color || cssValue.fontSize || cssValue.backgroundColor) {
-                if (elTag !== 'BR' && elTag !== 'I' && elTag !== 'IMG') {
-                    cssValue = JSON.stringify(cssValue).replace(/[{\"}]/g, '').replace(/,/g, ', ');
-                    htmlEl += `
-                        <p class="css-caption" style="position: absolute; font-size: 12px !important; color: #000 !important; background-color: rgba(0, 0, 0, 0.5) !important; margin-top: 0 !important;">
-                            ${cssValue}
-                        </p>
-                    `;
-                    $el.append(htmlEl);
-                }
-            }
-        });
-
-        $('.css-caption').each((idx, el) => {
-            removeCssCaption(el);
-
-            $(el)
-                .on('mouseenter', () => {
-                    $(el).css({ 'background-color': '#000', 'color': '#fff', 'padding': '4px 10px', 'z-index': '9999' });
-                })
-                .on('mouseleave', () => {
-                    $(el).css({ 'padding': '0', 'font-size': '12px !important', 'color': '#000 !important', 'background-color': 'rgba(0, 0, 0, 0.5) !important', 'margin-top': '0 !important', 'z-index': '' });
-                });
-        });
-
-        $('.tab > ul, .banner-group > .inr > .box a > span sub').css('overflow', 'visible');
-    }
-    function removeCssCaption(el) {
-        let elText = $(el).text().trim();
-        
-
-        while (true) {
-            const $parent = $(el).parent();
-            let elParentText = $parent.text().trim();
-
-            if ($parent.length == 0) {
-                return elText;
-            }
-
-            if(elText === elParentText){
-                $parent.remove();
-                el = $parent;
-            } else {
-                break;
-            }
-        };
-    }
-    // function removeCssCaption1(el){
-    //     let elText = $(el).text().trim();
-    //     let elParentText = $(el).parent().text().trim();
-
-    //     while (elText == elParentText) {
-    //         el = $(el).parent();
-    //         // elParentCss = $(target).css(property);
-
-    //         if (el.length == 0) {
-    //             return elText;
-    //         } else {
-    //             $(el).remove();
-    //         }
-    //     };
-    // }
-    
-    function rgbToHex(rgb) {
-        const rgbArray = rgb.match(/\d+/g);
-        return rgbArray ? `#${((1 << 24) + (parseInt(rgbArray[0]) << 16) + (parseInt(rgbArray[1]) << 8) + parseInt(rgbArray[2])).toString(16).slice(1)}` : null;
-    }
-
-    getAllStyle();
 });
 
 $(window).resize(function() {
@@ -515,3 +269,138 @@ $(window).resize(function() {
         }
     }
 });
+
+//임시
+$(document).ready(function(){
+	$('#wrap').before('<button type="button" class="btn xlg" id="btn-show">hide</button>');
+
+	getAllStyle();
+	btnShowClick();
+});
+
+function btnShowClick() {
+	$('#btn-show').on('click', (e) => {
+		let $this = $(e.currentTarget);
+
+		$this.toggleClass('hideBtn');
+		
+		$('.css-caption').each((idx, el) => {
+			if($this.hasClass('hideBtn')){
+				$(el).hide();
+				$this.text('show');
+			} else{
+				$(el).show();
+				$this.text('hide');
+			}
+		});
+	});
+}
+
+function getAllStyle() {
+	$('#wrap *').each((idx, el) => {
+		let htmlEl = '';
+		const $el = $(el);
+		const elTag = $el.prop('tagName');
+		const cssProperties = ['color', 'fontSize', 'backgroundColor'];
+		let cssValue = {};
+		const elHeight = $el.outerHeight();
+		
+		$.each(cssProperties, (idx, property) => {
+			const initialColor = $('body').css('color');
+			const initialFontSize = $('body').css('font-size');
+			const initialBackgroundColor = 'rgba(0, 0, 0, 0)';
+
+			if (property == 'color' && $(el).css(property) !== initialColor) {
+				cssValue[property] = rgbToHex($(el).css(property));
+			}
+			if (property == 'fontSize' && $(el).css(property) !== initialFontSize) {
+				cssValue[property] = $(el).css(property);
+			}
+			if (property == 'backgroundColor' && $(el).css(property) !== initialBackgroundColor) {
+				cssValue[property] = rgbToHex($(el).css(property));
+			}
+		});
+		if (cssValue.color || cssValue.fontSize || cssValue.backgroundColor) {
+			if (elTag !== 'BR' && elTag !== 'I' && elTag !== 'IMG' && $el.is(':visible')) {
+				cssValue = JSON.stringify(cssValue).replace(/[{\"}]/g, '').replace(/,/g, ', ');
+				htmlEl += `
+					<p class="css-caption" style="position: absolute; font-size: 12px !important; color: #000 !important; background-color: rgba(0, 0, 0, 0.5) !important; margin-top: 0 !important;">
+						${cssValue}
+					</p>
+				`;
+				$el.append(htmlEl);
+			}
+		}
+	});
+
+	$('.css-caption').each((idx, el) => {
+		// removeCssCaption(el, '.css-caption');
+		// let $el = $(el);
+		// let elText = $el.text().trim();
+		// let elParentText = $el.parent().children('.css-caption').text().trim();
+
+		// if(elText == elParentText){
+		// }
+
+		let parentUl = $(el).parents('ul');
+		if(parentUl){
+			parentUl.children('li').children('.css-caption').not(':first').remove();
+		}
+
+		$(el).parent()
+			.on('mouseenter', () => {
+				$(el).css({ 'background-color': '#000', 'color': '#fff', 'padding': '4px 10px', 'z-index': '9999' });
+			})
+			.on('mouseleave', () => {
+				$(el).css({ 'padding': '0', 'font-size': '12px !important', 'color': '#000 !important', 'background-color': 'rgba(0, 0, 0, 0.5) !important', 'margin-top': '0 !important', 'z-index': '' });
+			});
+	});
+
+	$('.tab > ul, .banner-group > .inr > .box a > span sub').css('overflow', 'visible');
+}
+function removeCssCaption(el, className) {
+	let elText = $(el).text().trim();
+	const textToRemove = elText;
+	// let elParentText = $(el).parent().children(className).text().trim();
+
+	while (true) {
+		const $parent = $(el).parent();
+		let elParentText = $parent.children(className).text().trim();
+
+		if ($parent.length == 0) {
+			break;
+			// return elText;
+		}
+
+		if(elText === elParentText){
+			// $parent.remove();
+			$(el).remove();
+			el = $parent;
+		} else {
+			break;
+		}
+	};
+	// while (elText === elParentText) {
+		
+	// 	// const $parent = $(el).parent();
+	// 	// let elParentText = $parent.text().trim();
+
+	// 	$(el).remove();
+	// 	// el = $parent;
+	// 	el = $(el).parent();
+
+	// 	if (elParentText.length == 0) {
+	// 		break;
+	// 		// return elText;
+	// 	}
+
+	// 	if(elText !== elParentText){
+	// 		break;
+	// 	}
+	// };
+}
+
+function rgbToHex(rgb) {
+	const rgbArray = rgb.match(/\d+/g);
+	return rgbArray ? `#${((1 << 24) + (parseInt(rgbArray[0]) << 16) + (parseInt(rgbArray[1]) << 8) + parseInt(rgbArray[2])).toString(16).slice(1)}` : null;
+}
